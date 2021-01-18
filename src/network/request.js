58cb0ac35b61a8d2c38f5d4312c07fcba72d8f2d
tanqingyao <qingyaotan@gmail.com>
2021-01-18 09:37:06 +0800

    '.gitignore'

--- src/network/request.js
+++
@@ -1,8 +0,0 @@
-import originAxios from "axios";
-
-export default function request(config) {
-  const instance = originAxios.create({
-    baseURL: "http://localhost:3300/"
-  });
-  return instance(config);
-}
