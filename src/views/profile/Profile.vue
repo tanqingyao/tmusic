6a734a4bd20681fc51decf5913a4e5f1b82b6b5d
tanqingyao <qingyaotan@gmail.com>
2021-01-18 11:31:42 +0800

    '添加profile自动播放选项'

--- src/views/profile/Profile.vue
+++ src/views/profile/Profile.vue
@@ -1,14 +1,42 @@
 <template>
   <div>
-    Profile
+    <ProfileNavBar title="用户设置" />
+    <label for="autoPlay">
+      选择歌曲时自动播放
+      <input
+        @input="autoPlaySetting"
+        type="checkbox"
+        v-model="isAutoPlay"
+        id="autoPlay"
+      />
+    </label>
+    <!-- <button :disabled="!isAutoPlay">下一步</button> -->
   </div>
 </template>
 <script>
+import ProfileNavBar from "./childComps/ProfileNavBar";
+
+import { SET_AUTOPLAY } from "store/mutations-types";
+import { mapMutations } from "vuex";
 export default {
   name: "Profile",
-  components: {},
+  components: {
+    ProfileNavBar
+  },
   data() {
-    return {};
+    return {
+      isAutoPlay: false
+    };
+  },
+  methods: {
+    ...mapMutations({
+      setAutoPlay: SET_AUTOPLAY
+    }),
+    autoPlaySetting(e) {
+      let state = e.target.checked;
+      this.isAutoPlay = state;
+      this.setAutoPlay(state);
+    }
   }
 };
 </script>
---
+++ src/views/profile/childComps/ProfileNavBar.vue
@@ -0,0 +1,43 @@
+<template>
+  <NavBar class="nav-bar">
+    <template #left>
+      <div class="back-icon" @click="backClick">
+        <img src="~assets/img/common/left_chevron_4.svg" alt="" />
+      </div>
+    </template>
+    <template #center>
+      <div class="text">{{ title }}</div>
+    </template>
+  </NavBar>
+</template>
+<script>
+import NavBar from "components/common/navbar/NavBar";
+export default {
+  name: "ProfileNavBar",
+  components: {
+    NavBar
+  },
+  props: {
+    title: {
+      type: String,
+      default: ""
+    }
+  },
+  methods: {
+    backClick() {
+      this.$router.back();
+    }
+  }
+};
+</script>
+<style scoped>
+.nav-bar {
+  color: var(--color-text);
+  font-size: var(--font-size-small-s);
+}
+.back-icon {
+  margin-top: 5px;
+  margin-left: 15px;
+  width: 30px;
+}
+</style>
